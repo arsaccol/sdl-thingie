4aff5a0d1c4ef818f657c6fff21e4603cc091432
Alexandre R. Saccol <arsaccol@gmail.com>
2023-09-29 02:25:10 -0300

    Add some matrix math I guess

--- math/Mat2x2.h
+++ math/Mat2x2.h
@@ -1,4 +1,5 @@
 #include "./Vector2.h"
+#include <cmath>
 
 namespace math {
     struct Mat2x2 {
@@ -10,15 +11,35 @@ namespace math {
         Mat2x2 operator+(Mat2x2);
 
         // matrix multiplication
-        Mat2x2 operator*(Mat2x2);
+        Mat2x2 operator*(const Mat2x2& other) const {
+            return Mat2x2 {
+                a11 * other.a11 + a12 * other.a21,      a11 * other.a12 + a12 * other.a22,
+                a21 * other.a11 + a22 * other.a21,      a21 * other.a12 + a22 * other.a22,
+            };
 
-        Mat2x2 operator*(const Vector2&);
+        }
 
-        static Vector2 Rotation(const Vector2&, const double);
-        
+        static Mat2x2 identity() {
+            return Mat2x2 {
+                1.0, 0.0,
+                0.0, 1.0
+            };
+        }
 
+        Vector2 operator*(const Vector2& vector) const {
+            return Vector2 {
+                a11 * vector.x + a12 * vector.x,
+                a21 * vector.y + a22 * vector.y,
+            };
 
+        }
 
+        static Mat2x2 rotationMatrix(const double angleRads) {
+            return Mat2x2{
+                std::cos(angleRads), -std::sin(angleRads),
+                std::sin(angleRads), std::cos(angleRads),
+            };
+        }
 
 
     };
